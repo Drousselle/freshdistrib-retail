01d9673f207d4b7e4f2334a8c33c88d4606f5d04
Dylan Rousselle <dylanrousselleaz@gmail.com>
2022-06-21 23:41:00 +0200

    refactor: adds slot number to Products, updates json accordingly

--- app/src/main/java/com/poin/freshdistrib/data/model/Products.kt
+++ app/src/main/java/com/poin/freshdistrib/data/model/Products.kt
@@ -4,5 +4,6 @@ package com.poin.freshdistrib.data.model
  *
  * Quantity val is a pair of the available quantity of the product and the unit (ml, g)
  *
+ * Slot is the slot number where the product is in the vending maching
  */
-data class Products(val name: String, val price: Double, val quantity: Pair<Int, String>)
+data class Products(val name: String, val price: Double, val quantity: Pair<Int, String>, val slot: Int)
